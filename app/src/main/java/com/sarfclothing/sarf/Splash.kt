6e22cce4e022055a04979bac995586c06b7c70ce
Ashancy <ashrikan@gmail.com>
2022-01-11 17:53:09 +0300

    new changes

---
+++ app/src/main/java/com/sarfclothing/sarf/Splash.kt
@@ -0,0 +1,19 @@
+package com.sarfclothing
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class Splash : AppCompatActivity() {
+    private val timeOut = 8000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, timeOut.toLong())
+    }
+}
